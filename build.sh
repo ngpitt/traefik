890ea410f70995f003bf875bf1a2f3140a2f09d8
Nick Pitt <ngpitt@xphysics.net>
2017-12-18 11:27:30 -0500

    Updated to 1.4.5

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 
-wget -O traefik https://github.com/containous/traefik/releases/download/v1.4.4/traefik_linux-arm
-wget -O ca-certificates.crt https://raw.githubusercontent.com/containous/traefik/v1.4.4/script/ca-certificates.crt
+wget -O traefik https://github.com/containous/traefik/releases/download/v1.4.5/traefik_linux-arm
+wget -O ca-certificates.crt https://raw.githubusercontent.com/containous/traefik/v1.4.5/script/ca-certificates.crt
 chmod +x traefik
--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-docker build -t ngpitt/traefik:v1.4.4 .
-docker push ngpitt/traefik:v1.4.4
+docker build -t ngpitt/traefik:v1.4.5 .
+docker push ngpitt/traefik:v1.4.5
